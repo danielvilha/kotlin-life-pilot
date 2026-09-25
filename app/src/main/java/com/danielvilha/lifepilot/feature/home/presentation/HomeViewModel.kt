d67e2429061fbde6d690b9770df1e1a3061aa6f3
Daniel Ferreira de Lima Vilha <danielvilha@gmail.com>
2026-09-25 11:54:49 +0100

    feat: improve home deletion state

--- app/src/main/java/com/danielvilha/lifepilot/feature/home/presentation/HomeViewModel.kt
+++ app/src/main/java/com/danielvilha/lifepilot/feature/home/presentation/HomeViewModel.kt
@@ -14,7 +14,6 @@ import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
